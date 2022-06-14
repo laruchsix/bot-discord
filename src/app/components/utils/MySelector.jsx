f4f65a0afee969cff398445526ebb34b27d10c77
trucher <61274375+laruchsix@users.noreply.github.com>
2022-06-14 03:27:38 +0200

    Update MySelector.jsx

--- src/app/components/utils/MySelector.jsx
+++ src/app/components/utils/MySelector.jsx
@@ -1,38 +1,48 @@
 import React from 'react';
 import Select from 'react-select';
 
-const MySelector = ({service, updateObjet, object}) => { 
+const MySelector = ({ service, updateObjet, isMulti = false, isDisabled = false, object, refresh = true, ...rest }) => {
     const [apiResponse, setApiResponse] = useState({
-        data: {},
-        loading: true,
-      });
-      useEffect(() => {
-        fetch(URL + service)
-          .then((res) => res.json())
-          .then((data) =>
-            setApiResponse({
-              data: data,
-              loading: false,
-            })
-          );
-      }, [service]);
-      
-      const handleChange = event => {
-          const value = event;
-          //console.log(value);
-          updateObjet(value);
-      }
-      if (apiResponse.loading) return <p>LOADING ...</p>;  
-      return (
-        <div style={{ width: "max-content", minWidth: "25vw"}}>
-          <ul style={{ margin: "1vw"}}>
-            <Select onChange={handleChange} options={apiResponse.data}  style={{ width: "max-content"}}/>
-          </ul>
-        </div>
-      );
-
-   
-
+      data: {},
+      loading: true,
+    });
+  
+    useEffect(() => {
+      fetch(URL + service)
+        .then((res) => res.json())
+        .then((data) => {
+          if (refresh) {
+            if (apiResponse !== data) updateObjet("");
+          }
+          //console.log(data);
+          setApiResponse({
+            data: data,
+            loading: false,
+          });
+        });
+    }, [service]);
+  
+    /**
+     * When the user changes the value of the input, update the state of the component with the new
+     * value.
+     */
+    const handleChange = (event) => {
+      const value = event;
+      updateObjet(value);
+    };
+  
+    /* A ternary operator. If the apiResponse.loading is true, it will return the first part of the
+    ternary operator. If it is false, it will return the second part of the ternary operator. */
+    if (apiResponse.loading) return <p>LOADING ...</p>;
+    return (
+      <div style={{ width: "100px", minWidth: "15vw" }}>
+        {isMulti ? (
+          <Select isMulti onChange={handleChange} value={object} options={apiResponse.data} style={{ width: "max-content" }} isDisabled={isDisabled} {...rest} />
+        ) : (
+          <Select onChange={handleChange} value={object} options={apiResponse.data} style={{ width: "max-content" }} isDisabled={isDisabled} isClearable {...rest} />
+        )}
+      </div>
+    );
 }
 
 
